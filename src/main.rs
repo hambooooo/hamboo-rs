2c1fed6cb2caa3007af2f05375b8bf23f4f62270
Michael <donghaiming@donghaiming.com>
2024-04-23 20:09:06 +0800

    触摸屏支持

--- src/main.rs
+++ src/main.rs
@@ -2,49 +2,42 @@
 #![no_main]
 
 
-use cst816s::{CST816S, TouchEvent, TouchGesture};
+use cst816s::{CST816S, TouchEvent};
 use display_interface_spi::SPIInterface;
 use embedded_graphics::{
     pixelcolor::Rgb565,
     prelude::*,
-    primitives::{Circle, Primitive, PrimitiveStyle, Rectangle, Triangle},
+    primitives::{Circle, Primitive, PrimitiveStyle, Rectangle},
 };
-use embedded_graphics::mono_font::ascii::FONT_8X13;
+use embedded_graphics::mono_font::ascii::FONT_10X20;
 use embedded_graphics::mono_font::MonoTextStyle;
 use embedded_graphics::text::Text;
 use embedded_hal_bus::spi::ExclusiveDevice;
 use esp_backtrace as _;
 use esp_hal::{
     clock::ClockControl,
-    delay,
     delay::Delay,
-    gpio::{IO, rtc_io::IntoLowPowerPin},
+    gpio::IO,
+    i2c::I2C,
     peripherals::Peripherals,
     prelude::*,
     spi::{master::Spi, SpiMode},
-    xtensa_lx::timer::delay,
 };
-use esp_hal::clock::CpuClock;
-use esp_hal::i2c::I2C;
-use mipidsi::{Builder, Display, options::ColorInversion};
-use mipidsi::models::ST7789;
-use mipidsi::options::{ColorOrder, Orientation, Rotation};
+use mipidsi::{
+    {Builder, options::ColorInversion},
+    models::ST7789,
+    options::ColorOrder,
+};
 
 const SCREEN_HEIGHT: u16 = 280;
-
 const SCREEN_WIDTH: u16 = 240;
-
-const SWIPE_LENGTH: u32 = 20;
-const SWIPE_WIDTH: u32 = 2;
 
 #[entry]
 fn main() -> ! {
     let peripherals = Peripherals::take();
     let system = peripherals.SYSTEM.split();
-
     let clocks = ClockControl::max(system.clock_control).freeze();
     let mut delay = Delay::new(&clocks);
-
     esp_println::logger::init_logger_from_env();
 
     let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
@@ -57,7 +50,7 @@ fn main() -> ! {
 
     bl.set_high();
 
-    let mut spi = Spi::new(
+    let spi = Spi::new(
         peripherals.SPI3,
         40u32.MHz(),
         SpiMode::Mode3,
@@ -70,7 +63,6 @@ fn main() -> ! {
     let di = SPIInterface::new(spi_device, dc);
     let mut display = Builder::new(ST7789, di)
         .reset_pin(rst)
-        // .orientation(Orientation::new().rotate(Rotation::Deg90))
         .display_size(SCREEN_WIDTH, SCREEN_HEIGHT)
         .display_offset(0, 20)
         .color_order(ColorOrder::Rgb)
@@ -84,31 +76,21 @@ fn main() -> ! {
     let touch_sda = io.pins.gpio11;
     let touch_scl = io.pins.gpio12;
 
-
-    let i2c = I2C::new(peripherals.I2C0, touch_sda, touch_scl, 100u32.kHz(), &clocks, None);
-    let mut touchpad = CST816S::new(i2c, touch_int, touch_rst);
-    touchpad.setup(&mut delay).unwrap();
+    let i2c = I2C::new(peripherals.I2C1, touch_sda, touch_scl, 400u32.kHz(), &clocks, None);
+    let mut touch = CST816S::new(i2c, touch_int, touch_rst);
+    touch.setup(&mut delay).unwrap();
 
     delay.delay(1.millis());
-
-    // Make the display all black
-    // display.clear(Rgb565::BLACK).unwrap();
-    // Draw a smiley face with white eyes and a red mouth
-    // draw_smiley(&mut display).unwrap();
-    // draw_hello_world(&mut display).unwrap();
+    draw_hello_world(&mut display).unwrap();
 
     loop {
-        draw_hello_world(&mut display);
-        delay.delay(2.secs());
-        draw_smiley(&mut display);
-        delay.delay(2.secs());
-        // if let Some(evt) = touchpad.read_one_touch_event(false) {
-        //     log::info!("{:?}",evt);
-        //
-        //     draw_marker(&mut display, &evt, Rgb565::RED);
-        // } else {
-        //     delay.delay(1.millis());
-        // }
+        if let Some(evt) = touch.read_one_touch_event(true) {
+            // log::info!("{:?}",evt);
+
+            draw_marker(&mut display, &evt, Rgb565::RED);
+        } else {
+            delay.delay(1.millis());
+        }
     }
 }
 
@@ -126,7 +108,7 @@ fn draw_hello_world<T: DrawTarget<Color=Rgb565>>(display: &mut T) -> Result<(), 
         .draw(display)?;
 
     Circle::new(Point::new(160, 0), 80)
-        .into_styled(PrimitiveStyle::with_fill(Rgb565::RED))
+        .into_styled(PrimitiveStyle::with_fill(Rgb565::CSS_GOLD))
         .draw(display)?;
     Circle::new(Point::new(160, 200), 80)
         .into_styled(PrimitiveStyle::with_fill(Rgb565::BLUE))
@@ -138,41 +120,9 @@ fn draw_hello_world<T: DrawTarget<Color=Rgb565>>(display: &mut T) -> Result<(), 
 
     Text::new(
         "Hello World!",
-        Point::new(80, 140),
-        MonoTextStyle::new(&FONT_8X13, RgbColor::WHITE),
+        Point::new(70, 140),
+        MonoTextStyle::new(&FONT_10X20, RgbColor::WHITE),
     )
-        .draw(display)?;
-    Ok(())
-}
-
-fn draw_smiley<T: DrawTarget<Color=Rgb565>>(display: &mut T) -> Result<(), T::Error> {
-    display.clear(Rgb565::BLACK)?;
-    // Draw the left eye as a circle located at (50, 100), with a diameter of 40, filled with white
-    Circle::new(Point::new(50, 80), 40)
-        .into_styled(PrimitiveStyle::with_fill(Rgb565::WHITE))
-        .draw(display)?;
-
-    // Draw the right eye as a circle located at (50, 200), with a diameter of 40, filled with white
-    Circle::new(Point::new(50, 180), 40)
-        .into_styled(PrimitiveStyle::with_fill(Rgb565::WHITE))
-        .draw(display)?;
-
-    // Draw an upside down red triangle to represent a smiling mouth
-    Triangle::new(
-        Point::new(130, 120),
-        Point::new(130, 180),
-        Point::new(160, 150),
-    )
-        .into_styled(PrimitiveStyle::with_fill(Rgb565::RED))
-        .draw(display)?;
-
-    // Cover the top part of the mouth with a black triangle so it looks closed instead of open
-    Triangle::new(
-        Point::new(130, 130),
-        Point::new(130, 170),
-        Point::new(150, 150),
-    )
-        .into_styled(PrimitiveStyle::with_fill(Rgb565::BLACK))
         .draw(display)?;
     Ok(())
 }
@@ -182,39 +132,9 @@ fn draw_marker(display: &mut impl DrawTarget<Color=Rgb565>, event: &TouchEvent, 
     let x_pos = event.x;
     let y_pos = event.y;
 
-    match event.gesture {
-        TouchGesture::SlideLeft | TouchGesture::SlideRight => {
-            Rectangle::new(
-                Point::new(x_pos - SWIPE_LENGTH  as i32, y_pos - SWIPE_WIDTH  as i32),
-                Size::new(x_pos as u32 + SWIPE_LENGTH, y_pos  as u32 + SWIPE_WIDTH),
-            )
-                .into_styled(PrimitiveStyle::with_fill(color))
-                .draw(display)
-                .map_err(|_| ())
-                .unwrap();
-        }
-        TouchGesture::SlideUp | TouchGesture::SlideDown => {
-            Rectangle::new(
-                Point::new(x_pos - SWIPE_LENGTH  as i32, y_pos - SWIPE_WIDTH  as i32),
-                Size::new(x_pos as u32 + SWIPE_LENGTH, y_pos  as u32 + SWIPE_WIDTH),
-            )
-                .into_styled(PrimitiveStyle::with_fill(color))
-                .draw(display)
-                .map_err(|_| ())
-                .unwrap();
-        }
-        TouchGesture::SingleClick => Circle::new(Point::new(x_pos, y_pos), 20)
-            .into_styled(PrimitiveStyle::with_fill(color))
-            .draw(display)
-            .map_err(|_| ())
-            .unwrap(),
-        TouchGesture::LongPress => {
-            Circle::new(Point::new(x_pos, y_pos), 40)
-                .into_styled(PrimitiveStyle::with_stroke(color, 4))
-                .draw(display)
-                .map_err(|_| ())
-                .unwrap();
-        }
-        _ => {}
-    }
+    Circle::new(Point::new(x_pos, y_pos), 5)
+        .into_styled(PrimitiveStyle::with_fill(color))
+        .draw(display)
+        .map_err(|_| ())
+        .unwrap();
 }
