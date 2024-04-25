1d3d77a8c4c37ee4e3c73b784cccfdb416cfa2eb
Michael <donghaiming@donghaiming.com>
2024-04-26 01:36:07 +0800

    slint

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,9 @@ extern crate alloc;
 
 #[cfg(not(feature = "simulator"))]
 use {
+    alloc::boxed::Box,
+    alloc::rc::Rc,
+    core::cell::RefCell,
     cst816s::{CST816S, TouchEvent},
     display_interface_spi::SPIInterface,
     embedded_graphics::{
@@ -28,10 +31,10 @@ use {
         peripherals,
         peripherals::Peripherals,
         prelude::*,
+        rtc_cntl::Rtc,
         spi::{master::Spi, SpiMode},
         systimer::SystemTimer,
         timer::TimerGroup,
-        rtc_cntl::Rtc,
     },
     mipidsi::{
         {Builder, options::ColorInversion},
@@ -70,145 +73,146 @@ fn main() -> ! {
     static ALLOCATOR: EspHeap = EspHeap::empty();
     unsafe { ALLOCATOR.init(&mut HEAP as *mut u8, core::mem::size_of_val(&HEAP)) }
 
-    let peripherals = Peripherals::take();
-    let system = peripherals.SYSTEM.split();
-    let clocks = ClockControl::max(system.clock_control).freeze();
-    let mut delay = Delay::new(&clocks);
-    esp_println::logger::init_logger_from_env();
-
-    let mut rtc = Rtc::new(peripherals.LPWR, None);
-
-    // Create timer groups
-    let timer_group0 = TimerGroup::new(peripherals.TIMG0, &clocks, None);
-    // Get watchdog timers of timer groups
-    let mut wdt0 = timer_group0.wdt;
-    let timer_group1 = TimerGroup::new(peripherals.TIMG1, &clocks, None);
-    let mut wdt1 = timer_group1.wdt;
-
-    // Disable watchdog timers
-    rtc.swd.disable();
-    rtc.rwdt.disable();
-    wdt0.disable();
-    wdt1.disable();
-
-    let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
-    let cs = io.pins.gpio16.into_push_pull_output();
-    let dc = io.pins.gpio17.into_push_pull_output();
-    let rst = io.pins.gpio13.into_push_pull_output();
-    let clk = io.pins.gpio15.into_push_pull_output();
-    let mosi = io.pins.gpio14.into_push_pull_output();
-    let mut bl = io.pins.gpio18.into_push_pull_output();
-
-    bl.set_high();
-
-    let spi = Spi::new(
-        peripherals.SPI3,
-        40u32.MHz(),
-        SpiMode::Mode3,
-        &clocks,
-    );
-    let spi = spi.with_sck(clk).with_mosi(mosi);
-    log::info!("spi init.");
-
-    let spi_device = ExclusiveDevice::new(spi, cs, delay);
-    let di = SPIInterface::new(spi_device, dc);
-    let mut display = Builder::new(ST7789, di)
-        .reset_pin(rst)
-        .display_size(240, 280)
-        .display_offset(0, 20)
-        .color_order(ColorOrder::Rgb)
-        .invert_colors(ColorInversion::Inverted)
-        .init(&mut delay)
-        .unwrap();
-    log::info!("display init.");
-
-    let touch_int = io.pins.gpio9.into_pull_up_input();
-    let touch_rst = io.pins.gpio10.into_push_pull_output();
-    let touch_sda = io.pins.gpio11;
-    let touch_scl = io.pins.gpio12;
-
-    let i2c = I2C::new(peripherals.I2C1, touch_sda, touch_scl, 400u32.kHz(), &clocks, None);
-    let mut touch = CST816S::new(i2c, touch_int, touch_rst);
-    touch.setup(&mut delay).unwrap();
-
     // Configure platform for Slint
-    let window = slint::platform::software_renderer::MinimalSoftwareWindow::new(Default::default());
+    slint::platform::set_platform(Box::new(SlintPlatform::default())).expect("backend already initialized");
 
-    slint::platform::set_platform(alloc::boxed::Box::new(SlintPlatform {
-        window: window.clone(),
-    }))
-        .unwrap();
+    create_slint_app().run().unwrap();
+    panic!("The MCU demo should not quit");
+}
 
-    struct SlintPlatform {
-        window: alloc::rc::Rc<slint::platform::software_renderer::MinimalSoftwareWindow>,
+#[cfg(not(feature = "simulator"))]
+#[derive(Default)]
+struct SlintPlatform {
+    window: RefCell<Option<Rc<slint::platform::software_renderer::MinimalSoftwareWindow>>>,
+}
+
+#[cfg(not(feature = "simulator"))]
+impl slint::platform::Platform for SlintPlatform {
+    fn create_window_adapter(
+        &self,
+    ) -> Result<alloc::rc::Rc<dyn slint::platform::WindowAdapter>, slint::PlatformError>
+    {
+        let window = slint::platform::software_renderer::MinimalSoftwareWindow::new(
+            slint::platform::software_renderer::RepaintBufferType::ReusedBuffer,
+        );
+        self.window.replace(Some(window.clone()));
+        Ok(window)
     }
 
-    impl slint::platform::Platform for SlintPlatform {
-        fn create_window_adapter(
-            &self,
-        ) -> Result<alloc::rc::Rc<dyn slint::platform::WindowAdapter>, slint::PlatformError>
-        {
-            Ok(self.window.clone())
-        }
-
-        fn duration_since_start(&self) -> core::time::Duration {
-            core::time::Duration::from_millis(
-                SystemTimer::now() / (SystemTimer::TICKS_PER_SECOND / 1000),
-            )
-        }
-
-        fn debug_log(&self, arguments: core::fmt::Arguments) {
-            esp_println::println!("{}", arguments);
-        }
+    fn duration_since_start(&self) -> core::time::Duration {
+        core::time::Duration::from_millis(
+            SystemTimer::now() / (SystemTimer::TICKS_PER_SECOND / 1000),
+        )
     }
 
-    let mut buffer_provider = DrawBuffer {
-        display,
-        buffer: &mut [slint::platform::software_renderer::Rgb565Pixel(0); 240],
-    };
+    fn debug_log(&self, arguments: core::fmt::Arguments) {
+        esp_println::println!("{}", arguments);
+    }
 
-    let _ui = create_slint_app();
+    fn run_event_loop(&self) -> Result<(), slint::PlatformError> {
+        let peripherals = Peripherals::take();
+        let system = peripherals.SYSTEM.split();
+        let clocks = ClockControl::max(system.clock_control).freeze();
+        let mut delay = Delay::new(&clocks);
+        esp_println::logger::init_logger_from_env();
 
-    let mut last_touch_action = None;
-    let mut last_touch_position = None;
+        let mut rtc = Rtc::new(peripherals.LPWR, None);
 
-    loop {
-        slint::platform::update_timers_and_animations();
-        window.draw_if_needed(|renderer| {
-            renderer.render_by_line(&mut buffer_provider);
-        });
+        // Create timer groups
+        let timer_group0 = TimerGroup::new(peripherals.TIMG0, &clocks, None);
+        // Get watchdog timers of timer groups
+        let mut wdt0 = timer_group0.wdt;
+        let timer_group1 = TimerGroup::new(peripherals.TIMG1, &clocks, None);
+        let mut wdt1 = timer_group1.wdt;
 
-        // TODO: Fix issue with short click. In this case only one interrupt is received and Pointer stays pressed.
-        let button = slint::platform::PointerEventButton::Left;
-        if let Some(event) = touch.read_one_touch_event(true).map(|record| {
-            let position = slint::PhysicalPosition::new(record.x as _, record.y as _)
-                .to_logical(window.scale_factor());
-            esp_println::println!("{:?}", record);
-            last_touch_position.replace(position);
-            last_touch_action.replace(record.action);
-            match record.action {
-                0 => WindowEvent::PointerPressed { position, button },
-                1 => WindowEvent::PointerReleased { position, button },
-                2 => WindowEvent::PointerMoved { position },
-                _ => WindowEvent::PointerExited,
+        // Disable watchdog timers
+        rtc.swd.disable();
+        rtc.rwdt.disable();
+        wdt0.disable();
+        wdt1.disable();
+
+        let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
+        let cs = io.pins.gpio16.into_push_pull_output();
+        let dc = io.pins.gpio17.into_push_pull_output();
+        let rst = io.pins.gpio13.into_push_pull_output();
+        let clk = io.pins.gpio15.into_push_pull_output();
+        let mosi = io.pins.gpio14.into_push_pull_output();
+        let mut bl = io.pins.gpio18.into_push_pull_output();
+
+        bl.set_high();
+
+        let spi = Spi::new(
+            peripherals.SPI3,
+            40u32.MHz(),
+            SpiMode::Mode3,
+            &clocks,
+        );
+        let spi = spi.with_sck(clk).with_mosi(mosi);
+        log::info!("spi init.");
+
+        let spi_device = ExclusiveDevice::new(spi, cs, delay);
+        let di = SPIInterface::new(spi_device, dc);
+        let mut display = Builder::new(ST7789, di)
+            .reset_pin(rst)
+            .display_size(240, 280)
+            .display_offset(0, 20)
+            .color_order(ColorOrder::Rgb)
+            .invert_colors(ColorInversion::Inverted)
+            .init(&mut delay)
+            .unwrap();
+        log::info!("display init.");
+
+        let touch_int = io.pins.gpio9.into_pull_up_input();
+        let touch_rst = io.pins.gpio10.into_push_pull_output();
+        let touch_sda = io.pins.gpio11;
+        let touch_scl = io.pins.gpio12;
+
+        let i2c = I2C::new(peripherals.I2C1, touch_sda, touch_scl, 400u32.kHz(), &clocks, None);
+        let mut touch = CST816S::new(i2c, touch_int, touch_rst);
+        touch.setup(&mut delay).unwrap();
+
+        let mut buffer_provider = DrawBuffer {
+            display,
+            buffer: &mut [slint::platform::software_renderer::Rgb565Pixel(0); 240],
+        };
+
+        loop {
+            slint::platform::update_timers_and_animations();
+
+            if let Some(window) = self.window.borrow().clone() {
+                window.draw_if_needed(|renderer| {
+                    renderer.render_by_line(&mut buffer_provider);
+                });
+                if window.has_active_animations() {
+                    continue;
+                }
             }
-        }) {
-            esp_println::println!("{:?}", event);
-            let is_pointer_release_event: bool =
-                matches!(event, WindowEvent::PointerReleased { .. });
-            window.dispatch_event(event);
 
-            // removes hover state on widgets
-            if is_pointer_release_event {
-                window.dispatch_event(WindowEvent::PointerExited);
-            }
-        }
-
-        if window.has_active_animations() {
-            continue;
+            // let button = slint::platform::PointerEventButton::Left;
+            // if let Some(event) = touch.read_one_touch_event(true).map(|record| {
+            //     let position = slint::PhysicalPosition::new(record.x as _, record.y as _)
+            //         .to_logical(self.window.scale_factor());
+            //     esp_println::println!("{:?}", record);
+            //     match record.action {
+            //         0 => WindowEvent::PointerPressed { position, button },
+            //         1 => WindowEvent::PointerReleased { position, button },
+            //         2 => WindowEvent::PointerMoved { position },
+            //         _ => WindowEvent::PointerExited,
+            //     }
+            // }) {
+            //     esp_println::println!("{:?}", event);
+            //     let is_pointer_release_event: bool = matches!(event, WindowEvent::PointerReleased { .. });
+            //     self.window.dispatch_event(event);
+            //
+            //     // removes hover state on widgets
+            //     if is_pointer_release_event {
+            //         self.window.dispatch_event(WindowEvent::PointerExited);
+            //     }
+            // }
         }
     }
 }
+
 #[cfg(not(feature = "simulator"))]
 struct DrawBuffer<'a, Display> {
     display: Display,
