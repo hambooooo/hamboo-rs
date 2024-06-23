7631824f38ea8841c6df503dc1d90386edb244c3
Michael <donghaiming@donghaiming.com>
2024-06-24 03:10:48 +0800

    convert image format

--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,32 @@ use image::io::Reader as ImageReader;
 fn main() -> Result<(), Box<dyn Error>> {
     let root_dir = std::env::current_dir().unwrap();
     let images_dir = root_dir.join("./ui/images");
-    let raw_image_path = images_dir.join("./face-pointer-hour.png");
-    let pxs_path = images_dir.join("./0001-face-pointer-hour.pxs");
 
-    let image = ImageReader::open(raw_image_path).unwrap().decode().unwrap();
+    let image_names = [
+        "face-pointer-hour",
+        "face-pointer-minute",
+        "face-pointer-second",
+        "app-calculate",
+    ];
 
-    println!("{}×{} image", image.width(), image.height());
-    let serializable_image = SerializableImage::new(image.width(), image.height(), image.as_bytes().to_vec());
-    let serialized_image = serializable_image.serialize();
-    println!("image path {}", pxs_path.clone().to_str().unwrap());
+    let mut index = 0;
+    for image_name in image_names {
+        index += 1;
+        let raw_image_path = images_dir.join(format!("./{}.png", image_name.clone()));
+        let pxs_path = images_dir.join(format!("./{:04}-{}.pxs", index, image_name.clone()));
 
-    let mut file = std::fs::File::create(pxs_path.clone()).unwrap();
-    file.write_all(serialized_image.as_slice()).unwrap();
-    println!("Successfully write bytes to file {:?}", pxs_path);
+        let image = ImageReader::open(raw_image_path).unwrap().decode().unwrap();
+
+        println!("{}×{} image", image.width(), image.height());
+        let serializable_image = SerializableImage::new(image.width(), image.height(), image.as_bytes().to_vec());
+        let serialized_image = serializable_image.serialize();
+        println!("image path {}", pxs_path.clone().to_str().unwrap());
+
+        let mut file = std::fs::File::create(pxs_path.clone()).unwrap();
+        file.write_all(serialized_image.as_slice()).unwrap();
+        println!("Successfully write bytes to file {:?}", pxs_path);
+    }
+
     Ok(())
 }
 
